925468d84d3449519088dc00db85065f829c4c14
Sashko <burdin009@gmail.com>
2019-03-14 17:14:01 +0300

    сделал нормальный случайный поиск и последовательный поиск

--- transfer_to_bits.py
+++ transfer_to_bits.py
@@ -58,9 +58,10 @@ def func_opt(num_blocks: list = [], blocks: list = [], opt = 1):
     otn_num_blocks = [x/total_case for x, total_case in zip(num_blocks, range_blocks)]
     for property in properties:
         global_func.append(sum([(x.__getattribute__(property)*rangs[property][1]*rangs[property][0]) * y for x, y in zip(blocks, otn_num_blocks)]))
-    print(sum(global_func))
     return (opt - sum(global_func))**2
 
+def find_opt(rangs):
+    return sum([val[0] * val[1] for val in rangs.values()])
 
 
 if __name__ == '__main__':
@@ -69,9 +70,9 @@ if __name__ == '__main__':
 
 
     num_blocks = [3,8, 2,10]
-    print(func_opt(num_blocks=num_blocks, blocks=transformed_blocks))
+    print(func_opt(num_blocks=num_blocks, blocks=transformed_blocks, opt=opt))
 
-    '''
+
     
     full_choice = {}
     for kg in range(1,4):
@@ -81,19 +82,11 @@ if __name__ == '__main__':
                     if sum([kg, kvs, rg, ip]) > total_case:
                         continue
                     temp_list = [str(kg), str(kvs), str(rg), str(ip)]
-                    full_choice[' '.join(temp_list)] = func_opt([kg, kvs, rg, ip], transformed_blocks)
+                    full_choice[' '.join(temp_list)] = func_opt([kg, kvs, rg, ip], transformed_blocks, opt)
     
-    print(full_choice)
-    
-    
-    best = ('random_best', 1111)
-    for k, v in full_choice.items():
-        if (opt - v)**2 < best[1]:
-            best = (k, (opt - v)**2)
-    
-    print(best[0], full_choice[best[0]])
-    
-    
+    full_choice_sorted = sorted(full_choice.items(), key=lambda kv: kv[1])
+    print(full_choice_sorted)
+
     #random test
     random_ch = {}
     for i in range(90000):
@@ -101,12 +94,7 @@ if __name__ == '__main__':
         if sum(num_blocks) > total_case:
             continue
         temp_list = [str(x) for x in num_blocks]
-        random_ch[' '.join(temp_list)] = func_opt(num_blocks, transformed_blocks)
-    
-    best = ('random_best', 1111)
-    for k, v in random_ch.items():
-        if (opt - v)**2 < best[1]:
-            best = (k, (opt - v)**2)
-    
-    print(best[0], random_ch[best[0]])
-    '''
+        random_ch[' '.join(temp_list)] = func_opt(num_blocks, transformed_blocks, opt)
+    random_ch_sorted = sorted(random_ch.items(), key=lambda kv: kv[1])
+    print(random_ch_sorted)
+
